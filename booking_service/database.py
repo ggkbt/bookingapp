e35de549f26ab03b78cab394fd0962b520961eee
Kurbatov Georgiy Gennadevich <kurbatovgg@psbank.ru>
2024-04-30 06:04:01 +0300

    fixed db connect (hopefully)

--- booking_service/database.py
+++ booking_service/database.py
@@ -1,12 +1,16 @@
 # /booking_service/database.py
+from copy import copy
 
 from booking_service.schemas.booking import Booking
-from sqlalchemy import create_engine
+from sqlalchemy.engine import create_engine, URL
 from sqlalchemy.orm import sessionmaker
 from booking_service.schemas.base_schema import Base
 from booking_service.settings import settings
 
-engine = create_engine(settings.postgres_url, echo=True)
+url = URL.create(drivername=settings.postgres_drivername, username=settings.postgres_user,
+                 password=settings.postgres_password, host=settings.postgres_host, port=settings.postgres_port,
+                 database=settings.postgres_database)
+engine = create_engine(url, echo=True)
 Base.metadata.create_all(engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- booking_service/settings.py
+++ booking_service/settings.py
@@ -5,8 +5,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     amqp_url: str
-    postgres_url: str
     room_service_url: str
+    postgres_drivername: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_port: str
+    postgres_database: str
 
     model_config = SettingsConfigDict(env_file='bookings.env')
 
