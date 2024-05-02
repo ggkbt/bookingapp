8846a47aed9ac38ddcd820440ffa3660d01268f8
Kurbatov Georgiy Gennadevich <kurbatovgg@psbank.ru>
2024-05-03 01:07:57 +0300

    added tests for booking-service

--- booking_service/tests/unit/test_booking_service.py
+++ booking_service/tests/unit/test_booking_service.py
@@ -15,13 +15,13 @@ def booking_service() -> BookingService:
 
 
 @pytest.fixture(scope='session')
-def first_booking_data() -> tuple[UUID, UUID, date, date]:
-    return uuid4(), uuid4(), date(2024, 5, 1), date(2024, 5, 3)
+def first_booking_data() -> tuple[UUID, date, date]:
+    return uuid4(), date(2024, 5, 1), date(2024, 5, 3)
 
 
 @pytest.fixture(scope='session')
-def second_booking_data() -> tuple[UUID, UUID, date, date]:
-    return uuid4(), uuid4(), date(2024, 5, 2), date(2024, 5, 4)
+def second_booking_data() -> tuple[UUID, date, date]:
+    return uuid4(), date(2024, 5, 2), date(2024, 5, 4)
 
 
 def test_empty_bookings(booking_service: BookingService) -> None:
@@ -29,32 +29,22 @@ def test_empty_bookings(booking_service: BookingService) -> None:
 
 
 def test_create_first_booking(
-        first_booking_data: tuple[UUID, UUID, date, date],
+        first_booking_data: tuple[UUID, date, date],
         booking_service: BookingService
 ) -> None:
-    booking_id, room_id, start_date, end_date = first_booking_data
+    room_id, start_date, end_date = first_booking_data
     booking = booking_service.create_booking(room_id, start_date, end_date)
-    assert booking.id == booking_id
     assert booking.room_id == room_id
     assert booking.start_date == start_date
     assert booking.end_date == end_date
     assert booking.status == BookingStatuses.CREATED
 
 
-def test_create_first_booking_repeat(
-        first_booking_data: tuple[UUID, UUID, date, date],
-        booking_service: BookingService
-) -> None:
-    _, room_id, start_date, end_date = first_booking_data
-    with pytest.raises(ValueError):
-        booking_service.create_booking(room_id, start_date, end_date)
-
-
 def test_create_second_booking(
-        second_booking_data: tuple[UUID, UUID, date, date],
+        second_booking_data: tuple[UUID, date, date],
         booking_service: BookingService
 ) -> None:
-    _, room_id, start_date, end_date = second_booking_data
+    room_id, start_date, end_date = second_booking_data
     booking = booking_service.create_booking(room_id, start_date, end_date)
     assert booking.room_id == room_id
     assert booking.start_date == start_date
@@ -63,22 +53,18 @@ def test_create_second_booking(
 
 
 def test_get_bookings_full(
-        first_booking_data: tuple[UUID, UUID, date, date],
-        second_booking_data: tuple[UUID, UUID, date, date],
+        first_booking_data: tuple[UUID, date, date],
+        second_booking_data: tuple[UUID, date, date],
         booking_service: BookingService
 ) -> None:
     bookings = booking_service.get_bookings()
     assert len(bookings) == 2
-    assert bookings[0].room_id == first_booking_data[1]
-    assert bookings[1].room_id == second_booking_data[1]
-
-
-def test_confirm_booking_status_error(
-        first_booking_data: tuple[UUID, UUID, date, date],
-        booking_service: BookingService
-) -> None:
-    with pytest.raises(ValueError):
-        booking_service.confirm_booking(first_booking_data[0])
+    assert bookings[0].room_id == first_booking_data[0]
+    assert bookings[0].start_date == first_booking_data[1]
+    assert bookings[0].start_date == first_booking_data[2]
+    assert bookings[1].room_id == second_booking_data[0]
+    assert bookings[1].start_date == second_booking_data[1]
+    assert bookings[1].start_date == second_booking_data[2]
 
 
 def test_confirm_booking_not_found(
@@ -89,19 +75,11 @@ def test_confirm_booking_not_found(
 
 
 def test_confirm_booking(
-        first_booking_data: tuple[UUID, UUID, date, date],
         booking_service: BookingService
 ) -> None:
-    booking = booking_service.confirm_booking(first_booking_data[0])
+    bookings = booking_service.get_bookings()
+    booking = await booking_service.confirm_booking(bookings[0].id)
     assert booking.status == BookingStatuses.CONFIRMED
-
-
-def test_complete_booking_status_error(
-        first_booking_data: tuple[UUID, UUID, date, date],
-        booking_service: BookingService
-) -> None:
-    with pytest.raises(ValueError):
-        booking_service.complete_booking(first_booking_data[0])
 
 
 def test_complete_booking_not_found(
@@ -112,19 +90,35 @@ def test_complete_booking_not_found(
 
 
 def test_complete_booking(
-        first_booking_data: tuple[UUID, UUID, date, date],
         booking_service: BookingService
 ) -> None:
-    booking = booking_service.complete_booking(first_booking_data[0])
+    bookings = booking_service.get_bookings()
+    booking = await booking_service.complete_booking(bookings[0].id)
     assert booking.status == BookingStatuses.COMPLETED
 
 
-def test_cancel_booking_status_error(
-        first_booking_data: tuple[UUID, UUID, date, date],
+def test_cancel_completed_booking_status_error(
         booking_service: BookingService
 ) -> None:
+    bookings = booking_service.get_bookings()
     with pytest.raises(ValueError):
-        booking_service.cancel_booking(first_booking_data[0])
+        booking_service.cancel_booking(bookings[0].id)
+
+
+def test_complete_completed_booking_status_error(
+        booking_service: BookingService
+) -> None:
+    bookings = booking_service.get_bookings()
+    with pytest.raises(ValueError):
+        booking_service.complete_booking(bookings[0].id)
+
+
+def test_confirm_completed_booking_status_error(
+        booking_service: BookingService
+) -> None:
+    bookings = booking_service.get_bookings()
+    with pytest.raises(ValueError):
+        booking_service.confirm_booking(bookings[0].id)
 
 
 def test_cancel_booking_not_found(
@@ -135,18 +129,32 @@ def test_cancel_booking_not_found(
 
 
 async def test_cancel_booking(
-        second_booking_data: tuple[UUID, UUID, date, date],
         booking_service: BookingService
 ) -> None:
-    booking = await booking_service.cancel_booking(second_booking_data[0])
+    bookings = booking_service.get_bookings()
+    booking = await booking_service.cancel_booking(bookings[1].id)
     assert booking.status == BookingStatuses.CANCELED
 
 
-def test_get_booking_by_id(
-        first_booking_data: tuple[UUID, UUID, date, date],
+def test_cancel_canceled_booking_status_error(
         booking_service: BookingService
 ) -> None:
-    booking = booking_service.get_booking_by_id(first_booking_data[0])
-    assert booking.room_id == first_booking_data[1]
-    assert booking.start_date == first_booking_data[2]
-    assert booking.end_date == first_booking_data[3]
+    bookings = booking_service.get_bookings()
+    with pytest.raises(ValueError):
+        booking_service.cancel_booking(bookings[1].id)
+
+
+def test_confirm_canceled_booking_status_error(
+        booking_service: BookingService
+) -> None:
+    bookings = booking_service.get_bookings()
+    with pytest.raises(ValueError):
+        booking_service.confirm_booking(bookings[1].id)
+
+
+def test_complete_canceled_booking_status_error(
+        booking_service: BookingService
+) -> None:
+    bookings = booking_service.get_bookings()
+    with pytest.raises(ValueError):
+        booking_service.complete_booking(bookings[1].id)
