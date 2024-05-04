07daeb6a365d0d9f55246ee897a3e041579469fd
Kurbatov Georgiy Gennadevich <kurbatovgg@psbank.ru>
2024-05-04 03:11:15 +0300

    added tests for room_service

--- room_service/settings.py
+++ room_service/settings.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     postgres_host: str
     postgres_port: str
     postgres_database: str
+    service_port: str
 
     model_config = SettingsConfigDict(env_file='rooms.env')
 
